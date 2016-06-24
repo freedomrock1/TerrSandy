6fb95b630585a1390f4ea54c63b2382c0d11c39a
Jack Le <Jack Le>
2016-06-23 21:27:25 -0400

    First commit

---
+++ PointFloat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wall_generator {
+    class PointFloat {
+        public float x = 0, y = 0;
+
+        public PointFloat(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
